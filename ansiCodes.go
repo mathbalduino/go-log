8a131af008501525623441120f86925228a37c34
Matheus Leonel Balduino <matheusl.balduino@gmail.com>
2021-06-17 15:15:08 -0300

    ansiCodes.go docs

--- ansiCodes.go
+++ ansiCodes.go
@@ -1,25 +1,50 @@
 package loxeLog
 
-// The codes that change the terminal color
+// The ANSI codes used to change the terminal color
 const (
-	terminalColorReset      = "\033[0m"
-	terminalColorRed        = "\033[31m"
-	terminalBoldColorRed    = "\033[31;1m"
-	terminalColorYellow     = "\033[33m"
-	terminalColorCyan       = "\033[36m"
-	terminalColorWhite      = "\033[37m"
-	terminalColorPurple     = "\033[35m"
-	terminalBoldColorPurple = "\033[35;1m"
+	ansiCodeReset   = "\033[0m"
+	ansiCodeRed     = "\033[31m"
+	ansiCodeBoldRed = "\033[31;1m"
+	ansiCodeYellow  = "\033[33m"
+	ansiCodeCyan    = "\033[36m"
+	ansiCodeWhite   = "\033[37m"
 )
 
-func RedString(msg string) string        { return colorizeString(msg, terminalColorRed) }
-func BoldRedString(msg string) string    { return colorizeString(msg, terminalBoldColorRed) }
-func YellowString(msg string) string     { return colorizeString(msg, terminalColorYellow) }
-func CyanString(msg string) string       { return colorizeString(msg, terminalColorCyan) }
-func WhiteString(msg string) string      { return colorizeString(msg, terminalColorWhite) }
-func PurpleString(msg string) string     { return colorizeString(msg, terminalColorPurple) }
-func BoldPurpleString(msg string) string { return colorizeString(msg, terminalBoldColorPurple) }
+// RedString will wrap the given string between the red and
+// reset ANSI codes.
+//
+// Terminals with ANSI code support will print the string to
+// the screen using red as the font color
+func RedString(msg string) string { return colorizeString(msg, ansiCodeRed) }
 
-// colorizeString will take some string and wrap it with
-// the selected terminal color code, colorizing it.
-func colorizeString(msg, color string) string { return color + msg + terminalColorReset }
+// BoldRedString will wrap the given string between the bold
+// red and reset ANSI codes.
+//
+// Terminals with ANSI code support will print the string to
+// the screen using bold red as the font color
+func BoldRedString(msg string) string { return colorizeString(msg, ansiCodeBoldRed) }
+
+// YellowString will wrap the given string between the yellow
+// and reset ANSI codes.
+//
+// Terminals with ANSI code support will print the string to
+// the screen using yellow as the font color
+func YellowString(msg string) string { return colorizeString(msg, ansiCodeYellow) }
+
+// CyanString will wrap the given string between the cyan and
+// reset ANSI codes.
+//
+// Terminals with ANSI code support will print the string to
+// the screen using cyan as the font color
+func CyanString(msg string) string { return colorizeString(msg, ansiCodeCyan) }
+
+// RedString will wrap the given string between the white and
+// reset ANSI codes.
+//
+// Terminals with ANSI code support will print the string to
+// the screen using white as the font color
+func WhiteString(msg string) string { return colorizeString(msg, ansiCodeWhite) }
+
+// colorizeString will wrap the given string between the given
+// color ANSI code and the reset ANSI code.
+func colorizeString(msg, color string) string { return color + msg + ansiCodeReset }
