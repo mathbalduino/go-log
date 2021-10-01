b39e008310e5dd0eb6163daa03e98e79d3231b5d
Matheus Leonel Balduino <matheusl.balduino@gmail.com>
2021-10-01 19:23:44 -0300

    Adjust site description

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
   return (
     <Layout
       title={siteConfig.title}
-      description="Description will go into a meta tag in <head />">
+      description="go-log library online documentation">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
