24a428a59821d4f4f90fe0b7b72c896439b77eb1
Matheus Leonel Balduino <matheusl.balduino@gmail.com>
2021-10-15 14:02:35 -0300

    Update docs edit path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/mathbalduino/go-log/edit/main/docs/',
+          editUrl: 'https://github.com/mathbalduino/go-log/edit/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
